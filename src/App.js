6f4d36af8614147116b3740555c100ad7682e411
Eunhye Ha <eh.ha@sqisoft.com>
2021-11-01 16:17:31 +0900

    Implement Tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { UserList } from "./users";
+
+import Dashboard from "./Dashboard";
+import authProvider from "./authProvider";
+
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App = () => (
+    <Admin dashboard={Dashboard}
+           authProvider={authProvider}
+           dataProvider={dataProvider}
+    >
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
+    </Admin>
+    );
 
 export default App;
---
+++ src/posts.js
@@ -0,0 +1,63 @@
+import * as React from "react";
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    SelectInput,
+    TextInput,
+} from 'react-admin';
+
+const postFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,
+    <ReferenceInput reference="users" source="userId" label="User" allowEmpty>
+        <SelectInput optionText="name" />
+    </ReferenceInput>,
+];
+
+export const PostList = props => (
+    <List filters={postFilters} {...props}>
+        <Datagrid>
+            <TextField source="id" />
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ``}</span>;
+};
+
+export const PostEdit = props => (
+    <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput reference="users" source="userId">
+                <SelectInput options="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
+);
